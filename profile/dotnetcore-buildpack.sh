6c91dfd22e4f96c8898c5b097ca964f135b12206
Duncan Leo <duncanleo97@gmail.com>
2017-07-10 13:06:25 +0800

    fix profile script creation

--- profile/dotnetcore-buildpack.sh
+++
@@ -1,3 +0,0 @@
-export FAKE_ROOT_DIR="$HOME/fake_root_dir"
-export LD_LIBRARY_PATH="$FAKE_ROOT_DIR/usr/lib/x86_64-linux-gnu:$FAKE_ROOT_DIR/usr/lib/i386-linux-gnu:$FAKE_ROOT_DIR/usr/lib:$LD_LIBRARY_PATH"
-export PATH="$HOME/dotnet/dotnet:$PATH"
